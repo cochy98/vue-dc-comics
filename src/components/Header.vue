319936bdf65440ca988b7787a923abe21bb8dd19
cochy98 <cosmoferrigno@gmail.com>
2022-03-28 16:41:32 +0200

    add project pages

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav>
+      <img src="../assets/img/dc-logo.png" alt="DC Logo" />
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Tv</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "IndexHeader",
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+/* nav {
+  display: flex;
+  justify-content: space-between;
+} */
+</style>
