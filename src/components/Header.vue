22345d43744a4643aa2f6e0149350b274c82be6e
cochy98 <cosmoferrigno@gmail.com>
2022-03-28 18:46:44 +0200

    Add header page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,26 @@
 <template>
   <header>
-    <nav>
-      <img src="../assets/img/dc-logo.png" alt="DC Logo" />
-      <ul>
-        <li><a href="#">Characters</a></li>
-        <li><a href="#">Comics</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">Tv</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Collectibles</a></li>
-        <li><a href="#">Videos</a></li>
-        <li><a href="#">Fans</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Shop</a></li>
-      </ul>
-    </nav>
+    <div class="container">
+      <nav>
+        <img
+          src="../assets/img/dc-logo.png"
+          class="header-logo"
+          alt="DC Logo"
+        />
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li class="active"><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Tv</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Collectibles</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -29,8 +35,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-/* nav {
+nav {
   display: flex;
   justify-content: space-between;
-} */
+  align-items: center;
+
+  .header-logo {
+    width: 70px;
+  }
+
+  ul {
+    list-style-type: none;
+
+    li {
+      display: inline-block;
+      height: 100px;
+      line-height: 100px;
+      margin: 0 1rem;
+      font-weight: 600;
+      font-size: 0.8rem;
+      text-transform: uppercase;
+
+      a {
+        text-decoration: none;
+        color: black;
+      }
+    }
+
+    li.active {
+      border-bottom: 2px solid #0282f9;
+    }
+
+    li.active a {
+      color: #0282f9;
+    }
+  }
+}
 </style>
